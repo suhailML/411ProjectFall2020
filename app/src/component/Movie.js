9a11822d2b9d7f14d9b12b69b697b7c127dd1ab2
Jared Min <jkymin2@gmail.com>
2020-12-11 12:48:27 -0500

    Add Movie.js, fix App.scss

--- app/src/component/Movie.js
+++ app/src/component/Movie.js
@@ -1,24 +1,73 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+class Movie extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            key: this.props.key,
+            isLoaded: false,
+            watched: false,
+            name: this.props.name,
+            id: this.props.movie.id,
+            poster: "",
+            runtime: 0,
+            info: [],
+        };
+    }
 
 /* example of stateless components -> components that are functions (functional),
 they take props in a param and then you can access the keys*/
-function expand() {
-    console.log("chicken");
+    expand() {
+        console.log("chicken");
+    }
 
-}
+    handleErr(err) {
+        console.warn(err);
+        let resp = new Response(
+          JSON.stringify({
+            code: 400,
+            message: "Bad Request"
+          })
+        );
+        return resp;
+    }
 
-const Movie = (props) => {
-    return (
-        <div>
-            <div className="trend-moviebox">
-                <button onClick={() => expand }> + </button>
-                <img src={props.poster} alt=""/>
+    componentDidMount() {
+        fetch(`https://api.themoviedb.org/3/movie/${this.state.id}?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&append_to_response=release_date,overview,poster_path,runtime`)
+                .then(response => response.json())
+                .then(results => {
+                    //console.log(results);
+                    this.setState({
+                        isLoaded: true,
+                        watched: false,
+                        poster: 'https://image.tmdb.org/t/p/w200' + results.poster_path,
+                        backdrop: 'https://image.tmdb.org/t/p/w200' + results.backdrop_path,
+                        runtime: results.runtime,
+                        release_date: results.release_date,
+                        info: results
+                    });
+                })
+                .catch(this.handleErr);
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="trend-moviebox">
+                    <button onClick={() => this.expand }> + </button>
+                    <img src={this.state.poster} alt=""/>
+                </div>
+                {this.state.name}
+                <br></br>
+                {`Runtime: ${Math.floor(this.state.runtime/60)} Hours  ${this.state.runtime%60} Minutes`}
+                <br></br>
+                {`Release Date: ${this.state.release_date}`}
             </div>
-            {props.name}
-                <p>movie genre here</p>
-        </div>
-            
-    );
+        );
+    }
 }
+
 
 export default Movie
