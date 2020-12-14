b6424955e997a8a5c17623f7606f4f2da266e583
rosolomo <rosolomo@DESKTOP-IO5VQJH.localdomain>
2020-12-13 20:49:47 -0500

    friedlist final i swear this time [FOR JARED :}]

--- app/server/db.js
+++ app/server/db.js
@@ -231,37 +231,35 @@ knex.schema
           console.error(`There was an error setting up the database: ${error}`)
         })
 
-            knex.schema
-            // Make sure no "books" table exists
-            // before trying to create new
-            .hasTable('friendList')
-              .then((exists) => {
-                if (!exists) {
-                  // If no "books" table exists
-                  // create new, with "id", "author", "title",
-                  // "pubDate" and "rating" columns
-                  // and use "id" as a primary identification
-                  // and increment "id" with every new record (book)
-                  return knex.schema.createTable('friendList', (table)  => {
-                    table.foreign('userId').references('userId').inTable('userInfo');
-                    table.foreign('friendId').references('userId').inTable('userInfo');
-                  })
-                  .then(() => {
-                    // Log success message
-                    console.log('Table \'FriendList \' created')
-                  })
-                  .catch((error) => {
-                    console.error(`There was an error creating table: ${error}`)
-                  })
-                }
+        knex.schema
+        // Make sure no "books" table exists
+        // before trying to create new
+        .hasTable('friendList')
+          .then((exists) => {
+            if (!exists) {
+
+              return knex.schema.createTable('friendList', (table)  => {
+                table.integer('user');
+                table.integer('friend');
+                table.foreign('user').references('id').inTable('userInfo');
+                table.foreign('friend').references('id').inTable('userInfo');
               })
               .then(() => {
                 // Log success message
-                console.log('done')
+                console.log('Table \'FriendList \' created')
               })
               .catch((error) => {
-                console.error(`There was an error setting up the database: ${error}`)
+                console.error(`There was an error creating table: ${error}`)
               })
+            }
+          })
+          .then(() => {
+            // Log success message
+            console.log('done')
+          })
+          .catch((error) => {
+            console.error(`There was an error setting up the database: ${error}`)
+          })
 
 // Just for debugging purposes:
 // Log all data in "books" table
